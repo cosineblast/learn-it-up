Fix missing dim in softmax for step selection

evaluation.py
@@ -200,7 +200,7 @@ def measure_selection_performance(model, chart, loss_fn, device):
         log_scores = torch.flatten(log_scores,start_dim=0, end_dim=1)
         y = torch.flatten(y)
 
-        predictions = torch.argmax(F.softmax(log_scores), dim=1).detach().cpu().numpy()
+        predictions = torch.argmax(F.softmax(log_scores, dim=1), dim=1).detach().cpu().numpy()
         mean_loss = torch.mean(loss_fn(log_scores, y)).detach().cpu().numpy()
 
     # computing main metrics 

selection_notebook.py
@@ -95,6 +95,8 @@ def _(device, models):
 
 @app.cell
 def _(nn, selection_model, torch):
+    # reduction=none is because we want the loss funciton to spit the loss for each element of the input sequence, 
+    # so we can apply the mask
     loss_fn = nn.CrossEntropyLoss(reduction='none')
     optimizer = torch.optim.Adam(selection_model.parameters(), lr=0.001, weight_decay=1e-4)
     return loss_fn, optimizer
@@ -123,7 +125,7 @@ def _(
 
             for batch, (x, deltas, y, mask) in enumerate(training_loader):
                 batch_size = x.shape[0]
-            
+
                 x = x.float().to(device)
                 deltas = deltas.float().to(device)
                 mask = mask.float().to(device)