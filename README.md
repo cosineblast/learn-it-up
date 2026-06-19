b6e92dadb14ba9b53586844ae50f181432246730
cosineblast <55855728+cosineblast@users.noreply.github.com>
2026-06-19 01:27:44 -0300

    Add documentation and improve gen.py
    - Seed support
    - Command line help messages
    - Running instructions
    - Device specification support

--- README.md
+++ README.md
@@ -1,12 +1,49 @@
 
-# learn it up
+# Learn it Up
 
 This project contains the (work in progress) code, and reference material for [Learn it Up](https://cosineblast.github.io/learn-it-up), a project that tries
-to adapt the LSTM and Transformer architectures for Pump it Up chart generation.
+to apply Machine Learning for Pump it Up chart generation.
 
-## File structure
+It currently implements an adaptation of the [DanceDanceConvolution](https://arxiv.org/abs/1703.06891) architecture with a CNN placement and LSTM placement models.
 
-This project assumes that the songs packs are in `./data/songs`, in usual `packdir/songdir` fashion. Step files must be in SSC.
+Developed and tested with python 3.11 and pytorch 2.6.0.
+
+## Running
+
+The dependencies for this project are listed in the file `pyproject.toml`.
+This project is compatible with `uv`.
+
+This project assumes that the song packs are in `./data/songs`, in usual `packdir/songdir` fashion. Step files must be in SSC.
+Chart `DESCRIPTION` attributes are assumed to represent the difficulties (`S1`...`S25`)
+
+Right now, this project does not work with stepfiles contaning StepP1 special notes such as vanishes, and charts including stop and warp gimmicks are ignored.
+
+Use `python3 main.py --help` to get a list of available operations.
+
+In order to train and run models, you will need to run the following operations:
+
+- `python3 main.py parse_all` to parse all SSC files into `data/parsed`.
+- `python3 main.py extract_all` to extract features from all audio files into `data/features`.
+- `python3 main.py partition` to create a `data/partitions.json` file containing the partitions of stepfiles that will be used for training, testing and validation.
+
+- In the notebook `python3 -m marimo edit onset_notebook.py` you will be able to train the step placement model.
+- Do the same with `python3 -m marimo edit selection_notebook.py` for the step selection model.
+
+Once you have the models trained, use `python3 gen.py` to generate your files.
+
+Example:
+
+```bash
+python3 gen.py samples/OshamaScramble.mp3 \
+  --difficulty S16 \
+  --selection selection.pth \
+  --placement onset.pth \
+  -o samples/oshama_scramble.ssc 
+```
+
+For more information, run `python3 gen.py --help`
+
+## Pipeline
 
 The processing pipeline consists of:
 
@@ -15,16 +52,6 @@ The processing pipeline consists of:
 - Introduce mirroring chart variations (vertical only for S17 and below)
 - Extract absolute time information for all charts
 - Save processed ssc files to disk, at path `./data/parsed/${PACK_NAME}___${SONG_TITLE}.ready.ssc.bin` (I wanted to use JSON but serializing and deserializing python classes to json sucks in general and i didnt want to add another dependency)
-
-## Running
-
-The dependencies for this project are listed in `pyproject.toml`.
-This project is compatible with `uv`.
-
-Use `python3 main.py --help` to get a list of available operations.
-
-- `python3 main.py parse_all` to parse all SSC files.
-- `python3 main.py extract_all` to extract features from all audio files.
 
 ## Notebooks
 
--- gen.py
+++ gen.py
@@ -16,7 +16,9 @@ def generate_stepfile(audio_path,
                       difficulty_str,
                       onset_model_path,
                       selection_model_path,
-                      result_path):
+                      result_path,
+                      device,
+                      seed=None):
 
     difficulty = check_difficulty(difficulty_str)
 
@@ -28,10 +30,12 @@ def generate_stepfile(audio_path,
     import models
     import numpy as np
 
+    if seed is not None:
+        torch.manual_seed(seed)
+
     print("Extracting audio features of {}... (JOOOOOOOO)".format(audio_path))
     loader = audio_util.AudioFeatureLoader(use_tqdm=True)
     features = loader.load(audio_path)
-    device = 'cpu'
 
     print("Loading onset model file {}... (AAAAE)".format(onset_model_path))
     onset_model_state = torch.load(onset_model_path, weights_only=False, map_location=torch.device(device))
@@ -82,7 +86,7 @@ def run_onset_model(features, onset_model, difficulty, device):
         scores = F.sigmoid(log_scores).detach().cpu().numpy()
     
     # TODO: save best threhold in alongside model
-    threshold = 0.2
+    threshold = BEST_THRESHOLDS[difficulty]
 
     window = np.hamming(5)
     onsets = np.convolve(scores, window, mode='same')
@@ -90,6 +94,36 @@ def run_onset_model(features, onset_model, difficulty, device):
     maxima = set(maxima)
 
     return sorted(onset / FRAMES_PER_SECOND for onset in maxima if scores[onset] > threshold)
+
+# Derived by inspecting the best onsets in onset_notebook.py
+BEST_THRESHOLDS = {
+    1: 0.09573398530483246,
+    2: 0.09573398530483246,
+    3: 0.19311115145683289,
+    4: 0.0552724227309227,
+    5: 0.26338380575180054,
+    6: 0.3407045900821686,
+    7: 0.1542433351278305,
+    8: 0.2203090637922287,
+    9: 0.2203090637922287,
+    10: 0.2543484568595886,
+    11: 0.19764167070388794,
+    12: 0.2639240026473999,
+    13: 0.23878812789916992,
+    14: 0.1600670963525772,
+    15: 0.29371264576911926,
+    16: 0.2168826311826706,
+    17: 0.21294879913330078,
+    18: 0.31380966305732727,
+    19: 0.22536088526248932,
+    20: 0.1528964787721634,
+    21: 0.22844624519348145,
+    22: 0.18381500244140625,
+    23: 0.1304573118686676,
+    24: 0.16991640627384186,
+    25: 0.16991640627384186,
+}
+    
 
 def run_selection_model(placements, selection_model, device):
     import torch.nn.functional as F
@@ -158,7 +192,7 @@ def build_stepfile(placements, steps, audio_path, result_path, difficulty_str):
     add('VERSION', '0.83')
     add('TITLE', 'unknown')
     add('SUBTITLE', '')
-    add('ARTIST', '')
+    add('ARTIST', 'PumpItUpConvolutionV0')
     add('TITLETRANSLIT', '')
     add('SUBTITLETRANSLIT', '')
     add('ARTISTTRANSLIT', '')
@@ -238,16 +272,23 @@ def main():
 
     args = parser.parse_args()
 
-    generate_stepfile(args.audio, args.difficulty, args.placement, args.selection, args.out)
+    generate_stepfile(args.audio, args.difficulty, args.placement, args.selection, args.out, args.device, args.seed)
 
 def cli_parser():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('audio')
-    parser.add_argument('-d', '--difficulty', required=True)
-    parser.add_argument('-p', '--placement', required=True)
-    parser.add_argument('-s', '--selection', required=True)
-    parser.add_argument('-o', '--out', required=True)
+    parser.add_argument('audio', help='Path of audio file to process')
+    parser.add_argument('-d', '--difficulty', required=True,
+                        help='Target difficulty of chart to generate (S1 up to S25)')
+    parser.add_argument('-p', '--placement', required=True,
+                        help='Path to the onset/step placement model to use')
+    parser.add_argument('-s', '--selection', required=True,
+                        help='Path to the step selection model to use')
+    parser.add_argument('-o', '--out', required=True,
+                        help='Path of .ssc file to generate')
+    parser.add_argument('--device', required=False, default='cpu',
+                        help='The pytorch device to use')
+    parser.add_argument('--seed', required=False, type=int, help='The seed to use. Pass none to use a random seed')
 
     return parser
 
